9d8b8895ce5af816018042bf312ef609544b3d02
ShocKKKKKKK <sergey.zhmursky@gmail.com>
2020-08-15 20:10:23 +0300

    add

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,7 @@ $(function () {
     slidesToShow: 2,
     arrows: true,
     variableWidth: true,
-    nextArrow: '<div class="slick-arrow slick-next"><img src="../img/portfolio/arrow.png"></div>',
-    prevArrow: '<div class="slick-arrow slick-prev"><img src="../img/portfolio/arrow.png"></div>',
+    nextArrow: '<div class="slick-arrow slick-next"><img src="img/portfolio/arrow.png"></div>',
+    prevArrow: '<div class="slick-arrow slick-prev"><img src="img/portfolio/arrow.png"></div>',
   });
 });
