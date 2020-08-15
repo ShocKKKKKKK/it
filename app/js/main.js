33ae2baf2798c099ff027791143bddc498b33be5
ShocKKKKKKK <sergey.zhmursky@gmail.com>
2020-08-15 19:24:56 +0300

    fs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
   $('.portfolio__slider').slick({
     slidesToShow: 2,
-    arrows: false,
+    arrows: true,
+    variableWidth: true,
+    nextArrow: '<div class="slick-arrow slick-next"><img src="../img/portfolio/arrow.png"></div>',
+    prevArrow: '<div class="slick-arrow slick-prev"><img src="../img/portfolio/arrow.png"></div>',
   });
 });
