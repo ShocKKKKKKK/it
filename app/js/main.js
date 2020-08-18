3bcf6ea8580146fa98f9112137c8fb1b371a0bbd
ShocKKKKKKK <sergey.zhmursky@gmail.com>
2020-08-18 21:12:07 +0300

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,11 @@ $(function () {
     variableWidth: true,
     nextArrow: '<div class="slick-arrow slick-next"><img src="img/portfolio/arrow.png"></div>',
     prevArrow: '<div class="slick-arrow slick-prev"><img src="img/portfolio/arrow.png"></div>',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
   });
 });
